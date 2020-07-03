db861e774a28853ab27014a84f665d2eda3e72b7
Kenji Koshikawa <koshikawa2009@gmail.com>
2020-07-03 22:45:43 +0900

    refs #4 fix settings for simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'bundler/setup'
 require 'tanita/api/client'
 require 'webmock/rspec'
